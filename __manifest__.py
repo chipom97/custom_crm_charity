2bc3ffa04a7872d801fbad820590597e6adb7d32
Chipo Mazhani <chipomazhani@gmail.com>
2025-05-15 12:34:35 +0200

    added and edited manifest file

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Custom CRM',
+    'version': '17.0',
+    'author': 'Automate Africa',
+    'depends': ['crm'],
+    'data': [
+      'views/views.xml',
+    ],
+
+    'sequence': 1,
+    'installable': True,
+    'application': True,
+
+}
