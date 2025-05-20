9852b3de1703491d4c6a60dd5924f381b189ced9
Chipo Mazhani <chipomazhani@gmail.com>
2025-05-20 11:49:00 +0200

    make contact-name invisible

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    incident_date = fields.Date(string="Date")
+    incident_date = fields.Char(string="Date")
     relationship = fields.Char(string="Relationship")
     id_number = fields.Char(string="ID Number")
     description = fields.Text(string="Description of Incident")
